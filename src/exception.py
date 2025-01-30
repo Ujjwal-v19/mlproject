7e48076988d9d660ff9c66b5f7d2647a0cb402ac
Ujjwal <ujjwalvarshney1111@gmail.com>
2025-01-30 23:49:43 +0530

    Data Ingestion Added

--- src/exception.py
+++ src/exception.py
@@ -1,11 +1,11 @@
 import sys
-from logger import logging
+from src.logger import logging
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()  #skipping starting 2 values 3rd is saved in exc_tb
     file_name = exc_tb.tb_frame.f_code.co_filename
     error_message = "Error Occured in Python Script name [{0}] line number [{1}] error message [{2}]".format(
         file_name,exc_tb.tb_lineno,str(error)
-    )
+    )   
     return error_message
 
 class CustomException(Exception):
